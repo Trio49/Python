77a15cf8bc605965f1f6042f1f7d074fe4ca0846
Trio49 <zachmusician@gmail.com>
2020-09-27 23:55:12 +0800

    Update expiry report.py

--- expiry report.py
+++ expiry report.py
@@ -1,9 +1,23 @@
-import pandas as pd
-import os
-#read file
+#split Expiry report by PMs and put in to tabs
 
-expiry=pd.read_csv('expiry.csv')
-#sorted by account code
-expiry=expiry.sort_values(by=['Account Code'])
-#count how many account has
-i=expiry['Account Code'].nunique()
+import openpyxl
+
+wb = openpyxl.load_workbook('Expiry.xlsx')
+ws = wb.active
+rngs = list(ws.values)
+d = {}
+for row in rngs[1:]:
+    if row[8] in d.keys():
+        d[row[8]] += [row]
+    else:
+        d[row[8]] = [row]
+
+nwb = openpyxl.Workbook()
+# key is PM , Position is Item
+for pm, position in sorted(d.items()):
+    nws=nwb.create_sheet(pm)  # create workbook
+    nws.append(rngs[0])
+    for expiry in position:
+        nws.append(expiry)
+nwb.remove(nwb.worksheets[0])
+nwb.save('Expirydetails.xlsx')
